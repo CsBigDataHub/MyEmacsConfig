d770881e5ec86eaf4b426abe41d4c3aad4d8e882
Chetan Koneru <chetan.koneru@optum.com>
2019-05-30 16:54:55 -0500

    Updated init and added custom functions

--- custom-el-scripts/custom-util-function.el
+++ custom-el-scripts/custom-util-function.el
@@ -59,3 +59,79 @@
     (if (not filename)
         (message "Buffer '%s' is not visiting a file!" name)
       (progn	(copy-file filename newname 1)	(delete-file filename)	(set-visited-file-name nil)))))
+
+(defun put-current-path-to-clipboard ()
+  (interactive)
+  (let ((file-path buffer-file-name)
+        (dir-path default-directory))
+    (cond (file-path
+           (kill-new (expand-file-name file-path))
+           (message "This file path is on the clipboard!"))
+          (dir-path
+           (kill-new (expand-file-name dir-path))
+           (message "This directory path is on the clipboard!"))
+          (t
+           (error-message-string "Fail to get path name.")))))
+
+(defun put-current-filename-to-clipboard ()
+  (interactive)
+  (let ((file-path buffer-file-name)
+        (dir-path default-directory))
+    (cond (file-path
+           (kill-new (file-name-nondirectory file-path))
+           (message "This file path is on the clipboard!"))
+          (dir-path
+           (kill-new (file-name-nondirectory dir-path))
+           (message "This directory path is on the clipboard!"))
+          (t
+           (error-message-string "Fail to get path name.")))))
+
+(defun put-current-filename-with-line-to-clipboard ()
+  (interactive)
+  (let ((file-path buffer-file-name)
+        (dir-path default-directory))
+    (cond (file-path
+           (kill-new (format "%s:%s"
+                             (file-name-nondirectory file-path)
+                             (count-lines (point-min) (point))))
+           (message "This file path is on the clipboard!"))
+          (dir-path
+           (kill-new (file-name-nondirectory dir-path))
+           (message "This directory path is on the clipboard!"))
+          (t
+           (error-message-string "Fail to get path name.")))))
+
+(defun upcase-backward-word (arg)
+  (interactive "p")
+  (upcase-word (- arg)))
+
+(defun downcase-backward-word (arg)
+  (interactive "p")
+  (downcase-word (- arg)))
+
+(defun capitalize-backward-word (arg)
+  (interactive "p")
+  (capitalize-word (- arg)))
+
+;;(global-set-key (kbd "C-M-u")	 'upcase-backward-word)
+;;(global-set-key (kbd "C-M-l")	 'downcase-backward-word)
+;;(global-set-key (kbd "M-c")	 'capitalize-backward-word)
+
+(defun kill-word-at-point ()
+  (interactive)
+  (let ((char (char-to-string (char-after (point)))))
+    (cond
+     ((string= " " char) (delete-horizontal-space))
+     ((string-match "[\t\n -@\[-`{-~],.、。" char) (kill-word 1))
+     (t (forward-char) (backward-word) (kill-word 1)))))
+
+;;(global-set-key (kbd "M-d")  'kill-word-at-point)
+
+(defun backward-kill-word-or-region (&optional arg)
+  (interactive "p")
+  (if (region-active-p)
+      (call-interactively #'kill-region)
+    (backward-kill-word arg)))
+
+;;(global-set-key (kbd "C-w")  'backward-kill-word-or-region)
+
