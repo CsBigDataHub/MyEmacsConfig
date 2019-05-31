64ff0d858dda8606e8a9bc87dfa3014917ad592a
Chetan Koneru <chetan.koneru@optum.com>
2019-05-31 09:20:09 -0500

    Added kill-whitespace function to custom-functions file.

--- custom-el-scripts/custom-util-function.el
+++ custom-el-scripts/custom-util-function.el
@@ -135,3 +135,13 @@
 
 ;;(global-set-key (kbd "C-w")  'backward-kill-word-or-region)
 
+(defun kill-whitespace ()
+  "Kill the whitespace between two non-whitespace characters"
+  (interactive "*")
+  (save-excursion
+    (save-restriction
+      (save-match-data
+        (progn
+          (re-search-backward "[^ \t\r\n]" nil t)
+          (re-search-forward "[ \t\r\n]+" nil t)
+          (replace-match "" nil nil))))))
