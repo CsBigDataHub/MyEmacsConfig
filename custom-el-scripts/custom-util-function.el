f54899cd9e639c636920c75f8193dfd1831691fd
Chetan Koneru <chetan.koneru@optum.com>
2019-06-03 13:10:33 -0500

    Updated few packages and functions

--- custom-el-scripts/custom-util-function.el
+++ custom-el-scripts/custom-util-function.el
@@ -1009,10 +1009,10 @@ region-end is used."
 
 
 (bind-keys*
-("M-m @" . my/hydra-of-macros/body))
+ ("M-m @" . my/hydra-of-macros/body))
 
 (defun my/smarter-move-beginning-of-line (arg)
-"Move point back to indentation of beginning of line.
+  "Move point back to indentation of beginning of line.
 
 Move point to the first non-whitespace character on this line.
 If point is already there, move to the beginning of the line.
@@ -1021,19 +1021,19 @@ the beginning of the line.
 
 If ARG is not nil or 1, move forward ARG - 1 lines first.  If
 point reaches the beginning or end of the buffer, stop there."
-(interactive "^p")
-(setq arg (or arg 1))
+  (interactive "^p")
+  (setq arg (or arg 1))
 
-;; Move lines first
-(when (/= arg 1)
-  (let ((line-move-visual nil))
-    (forward-line (1- arg))))
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
 
-(let ((orig-point (point)))
-  (back-to-indentation)
-  (when (= orig-point (point))
-    (move-beginning-of-line 1))))
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
 
 ;; remap C-a to `smarter-move-beginning-of-line'
 (global-set-key [remap move-beginning-of-line]
-'smarter-move-beginning-of-line)
+                'smarter-move-beginning-of-line)
