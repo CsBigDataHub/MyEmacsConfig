9dedae0f351f09bf13972ca84b0a35da48f5c2c1
Chetan Koneru <chetan.koneru@optum.com>
2019-09-11 16:16:52 -0500

    Added shell enhancements and functions to remove ^M's

--- custom-el-scripts/custom-util-function.el
+++ custom-el-scripts/custom-util-function.el
@@ -1865,6 +1865,23 @@ _n_ next-line          _S-SPC_ scroll-down-command              _d_ kill-buffer
 
 (global-set-key (kbd "C-x k") 'my/kill-this-buffer)
 
+(defun my/dos2unix ()
+  "Not exactly but it's easier to remember"
+  (interactive)
+  (set-buffer-file-coding-system 'unix 't) )
+
+(defun my/remove-control-M ()
+  "Remove ^M at end of line in the whole buffer."
+  (interactive)
+  (save-match-data
+    (save-excursion
+      (let ((remove-count 0))
+        (goto-char (point-min))
+        (while (re-search-forward (concat (char-to-string 13) "$") (point-max) t)
+          (setq remove-count (+ remove-count 1))
+          (replace-match "" nil nil))
+        (message (format "%d ^M removed from buffer." remove-count))))))
+
 (defun markdown-to-html ()
   "Compiles the current file to HTML using Pandoc."
   (interactive)
