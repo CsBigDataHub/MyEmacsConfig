ec774d542a20254189f73f0d5c9816463411f0ed
Chetan Koneru <chetan.koneru@optum.com>
2019-12-31 12:09:02 -0600

    Modified init.el with use-package ensure. fixed quelpa,cheat.
    commented ellocate too.

--- init.el
+++ init.el
@@ -9,8 +9,16 @@
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
-	(package-refresh-contents)
-	(package-install 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+;; for now accept that this is magic
+(eval-when-compile
+  (require 'use-package))
+;; get stable versions of packages unless otherwise specified
+;;(setq use-package-always-pin "melpa-stable")
+
+;; always make sure you have the package
+(setq use-package-always-ensure t)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 (put 'downcase-region 'disabled nil)
