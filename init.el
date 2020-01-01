7d04f79b75d7567568cbe71d7a3822ee66b0c7a5
CsBigDataHub <kchetan.hadoop@gmail.com>
2019-12-31 18:04:48 -0600

    modified init.el and myinit.org to handle quelpa

--- init.el
+++ init.el
@@ -11,4 +11,13 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+;; for now accept that this is magic
+(eval-when-compile
+  (require 'use-package))
+;; get stable versions of packages unless otherwise specified
+;;(setq use-package-always-pin "melpa-stable")
+
+;; always make sure you have the package
+(setq use-package-always-ensure t)
+
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
