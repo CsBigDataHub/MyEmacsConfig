9a7793a7c860de3eeb3e6f4a931a81e634e1ab41
Chetan Koneru <chetan.koneru@optum.com>
2020-02-10 14:08:52 -0600

    Updated dired hydra and added treemacs to general

--- custom-el-scripts/custom-util-function.el
+++ custom-el-scripts/custom-util-function.el
@@ -1067,7 +1067,7 @@ point reaches the beginning or end of the buffer, stop there."
 
 ;;Hydra for Dired
 (defhydra my/hydra-dired (:hint nil :color pink)
-  "
+	"
 _+_ mkdir          _v_iew                       _m_ark             _(_ details          _i_nsert-subdir      _w_dired
 _P_eep             _n_ filter                   _@_ mark regex     _<_ subtree-cycle    _>_ subtree-toggle
 _C_opy             _O_ view other               _U_nmark all       _)_ omit-mode        _$_ hide-subdir      C-x C-q : edit
@@ -1075,55 +1075,56 @@ _D_elete           _o_pen other                 _u_nmark           _l_ redisplay
 _R_ename-or-move   _M_ chmod                    _t_oggle           _g_ revert buf       _e_ ediff            C-c ESC : abort
 _Y_ rel symlink    _G_ chgrp                    _E_xtension mark   _s_ort               _=_ pdiff
 _S_ymlink          _fc_ copy-file-name          _F_ind marked      _._ toggle hydra     \\ flyspell
-_r_sync            _fp_ copy-file-name-path     ^ ^                ^ ^                  _?_ summary
-_z_ compress-file  _A_ find regexp
+_r_sync            _fp_ copy-file-name-path     _I_ Git Info       ^ ^                  _?_ summary
+_z_ compress-file  _A_ find regexp              ^ ^
 _Z_ compress       _Q_ repl regexp
 
 T - tag prefix
 "
-  ("\\" dired-do-ispell)
-  ("n" dired-narrow)
-  ("(" dired-hide-details-mode)
-  (")" dired-omit-mode)
-  ("+" dired-create-directory)
-  ("=" diredp-ediff)         ;; smart diff
-  ("?" dired-summary)
-  ("$" diredp-hide-subdir-nomove)
-  ("A" dired-do-find-regexp)
-  ("C" dired-do-copy)        ;; Copy all marked files
-  ("D" dired-do-delete)
-  ("E" dired-mark-extension)
-  (">" dired-subtree-toggle)
-  ("<" dired-subtree-cycle)
-  ("e" dired-ediff-files)
-  ("F" dired-do-find-marked-files)
-  ("G" dired-do-chgrp)
-  ("g" revert-buffer)        ;; read all directories again (refresh)
-  ("i" dired-maybe-insert-subdir)
-  ("l" dired-do-redisplay)   ;; relist the marked or singel directory
-  ("M" dired-do-chmod)
-  ("m" dired-mark)
-  ("O" dired-display-file)
-  ("o" dired-find-file-other-window)
-  ("P" peep-dired)
-  ("@" dired-mark-files-regexp)
-  ("Q" dired-do-find-regexp-and-replace)
-  ("R" dired-do-rename)
-  ("r" dired-do-rsynch)
-  ("S" dired-do-symlink)
-  ("s" dired-sort-toggle-or-edit)
-  ("t" dired-toggle-marks)
-  ("U" dired-unmark-all-marks)
-  ("u" dired-unmark)
-  ("v" dired-view-file)      ;; q to exit, s to search, = gets line #
-  ("w" dired-kill-subdir)
-  ("fc" dired-copy-filename-as-kill)
-  ("fp" dired-copy-file-path-as-kill)
-  ("Y" dired-do-relsymlink)
-  ("z" diredp-compress-this-file)
-  ("Z" dired-do-compress)
-  ("q" nil)
-  ("." nil :color blue))
+	("\\" dired-do-ispell)
+	("n" dired-narrow)
+	("(" dired-hide-details-mode)
+	(")" dired-omit-mode)
+	("+" dired-create-directory)
+	("=" diredp-ediff)         ;; smart diff
+	("?" dired-summary)
+	("$" diredp-hide-subdir-nomove)
+	("A" dired-do-find-regexp)
+	("C" dired-do-copy)        ;; Copy all marked files
+	("D" dired-do-delete)
+	("E" dired-mark-extension)
+	(">" dired-subtree-toggle)
+	("<" dired-subtree-cycle)
+	("e" dired-ediff-files)
+	("F" dired-do-find-marked-files)
+	("G" dired-do-chgrp)
+	("g" revert-buffer)        ;; read all directories again (refresh)
+	("i" dired-maybe-insert-subdir)
+	("I" dired-git-info-mode)
+	("l" dired-do-redisplay)   ;; relist the marked or singel directory
+	("M" dired-do-chmod)
+	("m" dired-mark)
+	("O" dired-display-file)
+	("o" dired-find-file-other-window)
+	("P" peep-dired)
+	("@" dired-mark-files-regexp)
+	("Q" dired-do-find-regexp-and-replace)
+	("R" dired-do-rename)
+	("r" dired-do-rsynch)
+	("S" dired-do-symlink)
+	("s" dired-sort-toggle-or-edit)
+	("t" dired-toggle-marks)
+	("U" dired-unmark-all-marks)
+	("u" dired-unmark)
+	("v" dired-view-file)      ;; q to exit, s to search, = gets line #
+	("w" dired-kill-subdir)
+	("fc" dired-copy-filename-as-kill)
+	("fp" dired-copy-file-path-as-kill)
+	("Y" dired-do-relsymlink)
+	("z" diredp-compress-this-file)
+	("Z" dired-do-compress)
+	("q" nil)
+	("." nil :color blue))
 
 (define-key dired-mode-map "." 'my/hydra-dired/body)
 
